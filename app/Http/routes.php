d08777b13a1c0f16b6e1f103658c6ae8e0353078
hiro <hiro.future07@gmail.com>
2017-03-26 08:08:56 +0000

    CRUD messages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
+
+
+// CRUD
+
+// Route::get('messages/{id}', 'MessagesController@show');
+// Route::post('messages', 'MessagesController@store');
+// Route::put('messages/{id}', 'MessagesController@update');
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// index: showの補助ページ
+// Route::get('messages/{id}', 'MessagesController@store');
+
+// create: 新規作成用のフォームページ
+// Route::get('messages/create', 'MessagesController@create');
+
+// edit: 更新用のフォームページ
+// Route::get('messages/{id}/edit', 'MessagesController@edit');
+

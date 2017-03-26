0f7b6417b4bfc6b10ecabb90d5cb2fda98f16949
hiro <hiro.future07@gmail.com>
2017-03-26 08:22:41 +0000

    add validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -43,6 +43,8 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, ['content' => 'required|max:255']);
+        
         $message = new Message;
         $message->content = $request->content;
         $message->save();
@@ -86,6 +88,8 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, ['content' => 'required|max:255']);
+        
         $message = Message::find($id);
         $message->content = $request->content;
         $message->save();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <title>MessageBoard</title>
     </head>
     <body>
+        @include('commons.error_messages')
+        
         @yield('content')
     </body>
 </html>

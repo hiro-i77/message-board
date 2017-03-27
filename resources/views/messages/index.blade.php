9d70381fb223c9e14a14cd0065ffe6e880c291eb
hiro <hiro.future07@gmail.com>
2017-03-27 11:09:10 +0000

    bootstrap design

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -5,16 +5,27 @@
     <h1>メッセージ一覧</h1>
     
     @if (count($messages) > 0)
-        <ul>
-            @foreach ($messages as $message)
-                <!-- 追加 -->
-                <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->title }} > {{ $message->content }}</li>
-            @endforeach
-        </ul>
-
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($messages as $message)
+                    <tr>
+                        <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                        <td>{{ $message->title }}</td>
+                        <td>{{ $message->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
     @if (count($messages) >= 0)
-        {!! link_to_route('messages.create', 'メッセージの新規作成') !!}
+        {!! link_to_route('messages.create', 'メッセージの新規作成', null, ['class' => 'btn btn-primary']) !!}
     @endif    
 
 @endsection
